fc020ea93f78b11bfee860d33f19d828f96b3a5e
ewilden <mew2@princeton.edu>
2016-06-29 17:51:36 -0700

    Update TimeFormatter.java

--- TimeFormatter.java
+++ TimeFormatter.java
@@ -7,7 +7,7 @@ import java.util.TimeZone;
 
 /**
  * Given a date String of the format given by the Twitter API, returns a display-formatted
- * String representing the relative time difference, e.g. "2m", "6d", "23 May", "1 Jan 2014"
+ * String representing the relative time difference, e.g. "2m", "6d", "23 May", "1 Jan 14"
  * depending on how great the time difference between now and the given date is.
  * This, as of 2016-06-29, matches the behavior of the official Twitter app.
  */
